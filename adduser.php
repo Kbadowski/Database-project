f79e52e4434284e1038756d754c277e01f26555a
Kelly Badowski <123491579+Kbadowski@users.noreply.github.com>
2024-12-03 12:37:39 -0500

    Update adduser.php

--- adduser.php
+++ adduser.php
@@ -1,35 +1,72 @@
 <?php
-//placeholder code
 // Initialize the session
 session_start();
-
-// Check if the user is logged in, if not then redirect him to login page
-
-//Define some constants in this PHP code block
 $servername = 'localhost';
 $username = 'kbadowsk'; // Flashline username
 $password = 'Gx0Vf7bb'; // phpMyAdmin password
 $dbname = 'kbadowsk'; // Flashline username
+
+// Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+ }
+//run a query to get all department names  
+$sqlstatement = $conn->prepare("SELECT distinct dept_name FROM department order by dept_name asc"); //prepare the statement
+$sqlstatement->execute(); //execute the query
+$departments = $sqlstatement->get_result(); //return the results we'll use them in the web form
+$sqlstatement->close();
+
+function displayFaculty() {
+  global $conn; //reference the global connection object (scope)
+  $sql = "SELECT * FROM student";
+        $result = $conn->query($sql);
+
+     if ($result->num_rows > 0) {
+        // Setup the table and headers
+        echo "<Center><table><tr><th>ID</th><th>Name</th><th>Department</th><th>Total_Credits</th></tr>";
+       // output data of each row into a table row
+       while($row = $result->fetch_assoc()) {
+           echo "<tr><td>".$row["ID"]."</td><td>".$row["name"]."</td><td> ".$row["dept_name"]."</td><td>$".$row["tot_cred"]."</td></tr>";
+           }
+      echo "</table></center>"; // close the table
+      echo "There are ". $result->num_rows . " results.";
+     // Don't render the table if no results found
+    } else {
+      echo "0 results";
+                                                                                                                      }
+ }
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Pantology </title>
+    <title>New Student</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
-                    table, th, td { border: 1px solid black; }
-        input[type=text] { width: 15%; padding: 12px 20px; margin: 8px 0;  box-sizing: border-box;}
-        input[type=button], input[type=submit], input[type=reset] { background-color: #04AA6D;  border: none;  color: white;  padding: 16px 32px;  text-decoration: none;  margin: 4px 2px;  cursor: pointer;}
+		table, th, td { border: 1px solid black; }
     </style>
 </head>
 <body>
-<p><h2>Pants look Up:</h2></p>
-<form action="pantslookup.php" method=get>
-        <p>Enter the Serial Number: <input type=text size=20 name="serial_num">
-        <p>Enter max price: <input type=text size=5 name="price">
-        <p> <input type=submit value="submit">
+New Student Entry Form:</h2></p>
+<form action="HW3data.php" method=get>
+	Enter Faculty name: <input type=text size=20 name="name">
+	<p>Enter Student ID number: <input type=text size=5 name="id">
+	<p>Select Student Department: 
+	<select name="department"
+        <?php //iterate through the results of the department query to build the web form
+	while($department = $departments->fetch_assoc()) {
+	?>
+		<option value="<?php echo $department["dept_name"]; ?>"><?php echo $department["dept_name"]; ?>
+		</option>
+	<?php } //end while loop ?>
+	</select> 
+
+  <p> <input type=submit value="submit">
                 <input type="hidden" name="form_submitted" value="1" >
 </form>
 
@@ -37,56 +74,36 @@ $dbname = 'kbadowsk'; // Flashline username
 <?php //starting php code again!
 if (!isset($_GET["form_submitted"]))
 {
-                echo "Hello. Please enter a Serial number or your max price and submit the form.";
+		echo "Hello. Please enter new Student information and submit the form.";
+    echo "<p>Here is a list of the current Student:";
+    displayFaculty();
 }
 else {
-// Create connection
+  if (!empty($_GET["name"]) && !empty($_GET["id"]))
+{
+   $studentName = $_GET["name"]; //gets name from the form
+   $studentID = $_GET["id"]; //gets id from the form
+   $studentDept = $_GET["department"]; //get department from the form
+   $studentcredits = 0; //get salary from the form
+   $sqlstatement = $conn->prepare("INSERT INTO student values(?, ?, ?, ?)"); //prepare the statement
+   if ($sqlstatement === false) {
+    die("SQL preparation failed: " . $conn->error);
+}
 
- $conn = new mysqli($servername, $username, $password, $dbname);
- // Check connection
- if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
- }
- if (!empty($_GET["serial_num"]))
- {
-   $serialnum = $_GET["serial_num"]; //gets name from the form
-   $sqlstatement = $conn->prepare("SELECT serial_num, price, stock FROM pants where serial_num LIKE ?"); //prepare the statement
-   $searchTerm = "%".$serialnum."%";
-   $sqlstatement->bind_param("s",$searchTerm); //insert the String variable into the ? in the above statement
+   $sqlstatement->bind_param("sssd",$studentID,$studentName,$studentDept,$studentcredits); //insert the variables into the ? in the above statement
    $sqlstatement->execute(); //execute the query
-   $result = $sqlstatement->get_result(); //return the results
-   $sqlstatement->close();
- }
- elseif (!empty($_GET["price"]))
- {
-   $pantsprice = $_GET["price"]; //gets name from the form
-   $sqlstatement = $conn->prepare("SELECT serial_num, price, stock FROM pants where price LIKE ?"); //prepare the statement
-   $searchTerm = "%".$pantsprice."%";
-   $sqlstatement->bind_param("s",$searchTerm); //insert the integer variable into the ? in the above statement
-   $sqlstatement->execute(); //execute the query
-   $result = $sqlstatement->get_result(); //return the results
+   echo $sqlstatement->error; //print an error if the query fails
    $sqlstatement->close();
  }
  else {
-         echo "<b>Please enter a serial number or max price  number</b>";
+	 echo "<b> Error: Please enter a name, an ID number and a salary to proceed.</b>";
  }
-   if ($result->num_rows > 0) {
-        // Setup the table and headers
-        echo "<center><table><tr><th>Serial Number</th><th>Price</th><th>stock</th></tr>";
-        // output data of each row into a table row
-         while($row = $result->fetch_assoc()) {
-                 echo "<tr><td>".$row["serial_num"]."</td><td>".$row["price"]."</td><td> ".$row["stock"]."</td></tr>";
-        }
-
-        echo "</table> </center>"; // close the table
-        echo "There are ". $result->num_rows . " results.";
-        // Don't render the table if no results found
-        } else {
-               echo "$name not found. 0 results";
-        }
+ 
+   echo "<p>Here is a list of the current Students:";
+   displayFaculty();
    $conn->close();
  } //end else condition where form is submitted
- ?> <!-- this is the end of our php code -->
-<p> Thanks for using the directory search!
+  ?> <!-- this is the end of our php code -->
 </body>
+
 </html>
